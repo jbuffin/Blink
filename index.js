442c41a75d74d8b267cc146efff013a2b4c1aa4f
Jason Buffin <jason@tackk.com>
2015-05-08 15:23:11 -0400

    formatting

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function(msg) {
     socket.broadcast.emit('chat message', msg);
   });
 });
