fb21af66b1d85d06ec89d4fa6cf33c2f9f4d7e04
Jason Buffin <jason@tackk.com>
2015-05-19 16:42:27 -0400

    new `Message` factory function to build the message handlers

---
+++ Message.js
@@ -0,0 +1,27 @@
+'use strict';
+var Request = require('request');
+var Utils = require('../utils');
+
+var messageHandlers = {
+  comment: function(opts) {
+    var options = {
+      uri: Utils.buildWinkUrl('/streams/'+this.options.room+'/comments'),
+      qs: {access_token:this.options.accessToken},
+      json: {comment:this.message.data.comment}
+    };
+
+    Request.post(options, function(error, response, body) {
+      this.socket.broadcast.to(this.options.room).emit('message', {type:'comment', data:body.data});
+    }.bind(this));
+  }
+};
+
+function Message(opts) {
+  return {
+    message: opts.message,
+    handleMessage: messageHandlers[opts.message.type],
+    socket: opts.socket,
+    options: opts
+  };
+}
+module.exports = Message;
