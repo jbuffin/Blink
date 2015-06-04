db82211cc44c8af81f4da7fa7b567becbf76045d
Jason Buffin <jason@tackk.com>
2015-06-04 16:25:05 -0400

    use EventsController to handle the /events api
    Note: still passing `emitter` this is going to change

--- config/routes.js
+++ config/routes.js
@@ -1,20 +1,10 @@
 var Controllers = require('../App/Controllers');
-var Utils = require('../utils');
 var routes = {
   init: function(app, emitter) {
     app.get('/', Controllers.RootController.getHomePage);
 
     app.post('/events', function(req, res) {
-      if(Utils.checkAuth(req.body.api_key)) {
-        if(emitter) {
-          emitter.to(req.body.room).emit('message', req.body);
-          res.json({ok:true});
-        } else {
-          res.json({ok:false, message: 'Was not able to find redis'});
-        }
-      } else {
-        res.json({ok:false, message: 'Invalid API Key'});
-      }
+      Controllers.EventsController.handleIncomingEvent(req, res, emitter);
     });
   }
 }
