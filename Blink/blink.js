1b3d95eb7e55efaff207f2365968bfc440ab793a
Jason Buffin <jason@tackk.com>
2015-06-04 18:47:04 -0400

    listen for the new Socket object's disconnect

--- Blink/blink.js
+++ Blink/blink.js
@@ -6,18 +6,25 @@ var Blink = function BlinkConstructor() {
   if (config.redis) {
     this.emitter = require('socket.io-emitter')(config.redis);
   }
+  this.sockets = [];
 };
+
 Blink.prototype.listen = function listen(Server) {
   this.io = require('socket.io')(Server);
   if(config.redis) {
     this.io.adapter(require('socket.io-redis')(config.redis));
   }
-
-  this.io.on('connection', function(socket) {
-    new Socket(socket);
-  });
+  this.io.on('connection', this.newSocket.bind(this));
 
   return this;
+};
+
+Blink.prototype.newSocket = function newSocket(socket) {
+  var newSocket = new Socket(socket);
+  var index = this.sockets.push(newSocket)-1;
+  newSocket.on('disconnect', function() {
+    this.sockets.splice(index, 1);
+  }.bind(this));
 };
 
 module.exports = new Blink();
