ec496ade44b631bce7d15ec22b2418fc327c3d95
Jason Buffin <jason@tackk.com>
2015-06-04 16:24:18 -0400

    `EventsController` method to handle events coming from API

--- App/Controllers/EventsController.js
+++ App/Controllers/EventsController.js
@@ -1,5 +1,18 @@
+var Utils = require('../../utils');
 var EventsController = function() {
 
+};
+EventsController.prototype.handleIncomingEvent = function handleIncomingEvent(req, res, emitter) {
+  if(Utils.checkAuth(req.body.api_key)) {
+    if(emitter) {
+      emitter.to(req.body.room).emit('message', req.body);
+      res.json(Utils.buildResponseJson(true));
+    } else {
+      res.json(Utils.buildResponseJson(false, 'Was not able to find redis'));
+    }
+  } else {
+    res.json(Utils.buildResponseJson(false, 'Invalid API Key'));
+  }
 };
 
 module.exports = new EventsController();
