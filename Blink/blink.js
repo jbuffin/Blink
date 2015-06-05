6cc5c9f934de649d934734a0571a2499c295dcc3
Jason Buffin <jason@tackk.com>
2015-06-05 10:02:47 -0400

    no need for emitter directly to redis

--- Blink/blink.js
+++ Blink/blink.js
@@ -3,10 +3,7 @@ var config = require('../config');
 var Socket = require('./socket');
 
 var Blink = function BlinkConstructor() {
-  if (config.redis) {
-    this.emitter = require('socket.io-emitter')(config.redis);
-  }
-  this.sockets = [];
+  this._sockets = [];
 };
 
 Blink.prototype.listen = function listen(Server) {
@@ -21,20 +18,16 @@ Blink.prototype.listen = function listen(Server) {
 
 Blink.prototype.newSocket = function newSocket(socket) {
   var newSocket = new Socket(socket);
-  var index = this.sockets.push(newSocket)-1;
+  var index = this._sockets.push(newSocket)-1;
   newSocket.on('disconnect', function() {
     console.log('user disconnected');
-    this.sockets.splice(index, 1);
+    this._sockets.splice(index, 1);
   }.bind(this));
 };
 
 Blink.prototype.externalMessage = function externalMessage(data) {
-  if(this.emitter) {
-    this.emitter.to(data.room).emit('message', data);
-    return true;
-  } else {
-    return false;
-  }
+  this.io.to(data.room).emit('message', data);
+  return true;
 };
 
 module.exports = new Blink();
