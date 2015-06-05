a9000f1ee1b07673e126ec35fc9412ffc001429f
Jason Buffin <jason@tackk.com>
2015-06-05 09:47:01 -0400

    Pass the message to Blink

--- App/Controllers/EventsController.js
+++ App/Controllers/EventsController.js
@@ -1,13 +1,13 @@
 'use strict';
 var Utils = require('../../utils');
-var emitter = require('../../Blink').emitter;
+var Blink = require('../../Blink');
 var EventsController = function EventsControllerConstructor() {
 
 };
+
 EventsController.prototype.handleIncomingEvent = function handleIncomingEvent(req, res) {
   if(Utils.checkAuth(req.body.api_key)) {
-    if(emitter) {
-      emitter.to(req.body.room).emit('message', req.body);
+    if(Blink.externalMessage(req.body)) {
       res.json(Utils.buildResponseJson(true));
     } else {
       res.json(Utils.buildResponseJson(false, 'Was not able to find redis'));
