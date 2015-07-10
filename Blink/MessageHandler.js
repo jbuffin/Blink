2747e2e2bb231ee9767ca8592f76e46da210695a
Jason Buffin <jason@tackk.com>
2015-07-10 08:45:06 -0400

    better debug logging

--- Blink/MessageHandler.js
+++ Blink/MessageHandler.js
@@ -127,7 +127,8 @@ function request(options) {
         }
       }.bind(this));
     } else {
-      console.error('There was an error: ', options);
+      console.error('There was an error: ', response.body);
+      console.error('options:', options);
       console.error('Request Type:', options.requestType == requestTypes.POST ? 'POST' : (options.requestType == requestTypes.PUT ? 'PUT' : ''));
       console.error(error);
     }
